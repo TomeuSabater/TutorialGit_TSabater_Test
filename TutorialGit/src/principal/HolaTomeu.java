637552060f677a81ce656e65e68d7c2c724874f6
Usu2 Tomeu <usu2@example.com>
2024-12-15 17:39:49 +0100

    Se añade titulación

--- TutorialGit/src/principal/HolaTomeu.java
+++ TutorialGit/src/principal/HolaTomeu.java
@@ -5,13 +5,15 @@ public class HolaTomeu {
 	public static void main(String[] args) {
 
 
-		final  String empresa = "CESUR";
+		final  String EMPRESA = "CESUR";
+		
 		String nombre = "Tomeu";
 		String modulo = "Entornos de Desarrollo"; 
-		
-		
-		System.out.println("Hola " + nombre + " que trabajas en " + empresa); 
+		String titulacion = "DAM/DAW"; 
+				
+		System.out.println("Hola " + nombre + " que trabajas en " + EMPRESA); 
 		System.out.println("Y que das clase del módulo " + modulo); 
+		System.out.println("De la titulación " + titulacion); 
 
 	}
 
