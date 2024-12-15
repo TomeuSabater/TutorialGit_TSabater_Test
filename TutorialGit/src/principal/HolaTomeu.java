bb93ab6707260a466dacc89505fa64e7e8af925e
Usu2 Tomeu <usu2@example.com>
2024-12-15 17:30:53 +0100

    Se inicia el proyecto

---
+++ TutorialGit/src/principal/HolaTomeu.java
@@ -0,0 +1,14 @@
+package principal;
+
+public class HolaTomeu {
+
+	public static void main(String[] args) {
+
+		String nombre = "Tomeu";
+		String empresa = "CESUR";
+		
+		System.out.println("Hola " + nombre + " que trabajas en " + empresa); 
+
+	}
+
+}
