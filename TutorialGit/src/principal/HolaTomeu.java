f47bb6867486de2447ff553dff42fd0826cce645
Usu2 Tomeu <usu2@example.com>
2024-12-15 17:36:57 +0100

    Mejoras sobre el código original

--- TutorialGit/src/principal/HolaTomeu.java
+++ TutorialGit/src/principal/HolaTomeu.java
@@ -4,10 +4,14 @@ public class HolaTomeu {
 
 	public static void main(String[] args) {
 
+
+		final  String empresa = "CESUR";
 		String nombre = "Tomeu";
-		String empresa = "CESUR";
+		String modulo = "Entornos de Desarrollo"; 
+		
 		
 		System.out.println("Hola " + nombre + " que trabajas en " + empresa); 
+		System.out.println("Y que das clase del módulo " + modulo); 
 
 	}
 
